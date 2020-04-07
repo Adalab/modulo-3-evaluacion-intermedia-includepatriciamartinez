ca0cc12994d9b348bde4b71f023f6039b0aa8a1a
includepatriciamartinez <includepatriciamartinez@gmail.com>
2020-04-07 10:27:17 +0200

    Update docs

---
+++ docs/precache-manifest.f93fb7deb44b761556109e368183294e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0be05b8dd72a6f4b49fb318d0935ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8e68bc59607ac8329491",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3dc708108b853815bca7",
+    "url": "./static/js/2.75c4a900.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.75c4a900.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e68bc59607ac8329491",
+    "url": "./static/js/main.3e5359b4.chunk.js"
+  },
+  {
+    "revision": "fa5b70473673cae622f8",
+    "url": "./static/js/runtime-main.f475111d.js"
+  }
+]);
